5ed79307f4b95cc9596e33d1f11a0138620b313f
Potequi <carolzinhapotequi@gmail.com>
2025-05-14 00:05:42 +0000

    criação de usuário

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import { mainModule } from "process";
+import { sequelize } from "./config/database";
+import { userInfo } from "os";
 
 const app = express();
 const port = 3000;
@@ -7,4 +10,23 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+
+
+main()
+
+async function main() {
+  if (require.main === module) {
+    await sequelize.sync({force: true}).then(() => {
+    console.log("Database is synced");
+    app.listen(port, ()=>
+    console.log('listening at http://localhost:${port}'),
+  );
+  
+  });
+
+
+  //criar usuario
+  const user = await user.create({ name: 'alice', email: 'alice@email,com'});
+  console.log("Usuario criado:", user.toJSON());
+});
+
