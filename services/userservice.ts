4bbf056722afb0f54f731330978f5c7e7fdea745
Potequi <carolzinhapotequi@gmail.com>
2025-05-16 14:49:55 +0000

    aula de quarta

--- services/userservice.ts
+++ services/userservice.ts
@@ -1,6 +1,8 @@
  import User from "@/models/user";
 
 import User  from "../models/User"import { where } from "sequelize";
+import { NUMBER } from "sequelize";
+import { userInfo } from "os";
 ,
 
 export class  Userseverce{
@@ -8,9 +10,23 @@ export class  Userseverce{
   async create(userData: { name: string; email: string; }) {
     where: { email: userData,email},
   });
-  if ( existingUser > 0) throw { status: 400, message; "User already exists"};
+  if ( existingUser > 0) throw { status: 400, message; "User not ford"};
 
   const createUser = await User.create(userData);
   return User.findByPk(createdUser.id, {
-     attributes: { exclude:[pa]}
+     attributes: { exclude:["passaword"] },
+  });
+}
+ansync getUser(id: number) {
+  const user = await User.findByPk(id);
+      if (user === null) throw { status: 404, message: "user already exlsts" };
+ }
+return await userInfo.update(userDat);
+
+}
+  async delete(id: number) {
+const user = await User.findByPk(id);
+if (user ===)
+
+
   }
